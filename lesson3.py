5b71f6873d013b3fa9ad61e6118980a27893bcd9
dima_kan <kandaurovdm@gmail.com>
2026-08-25 20:18:37 +0200

    20.08

--- lesson3.py
+++ lesson3.py
@@ -1,136 +1,223 @@
-import os
 import dotenv
+import os
 
-from typing import List
+import langchain
 from pydantic import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 
 
-# завантаження апі ключа
 dotenv.load_dotenv()
+
 api_key = os.getenv("GEMINI_API_KEY")
 
-# створити llm
+
+# # модель
 llm = ChatGoogleGenerativeAI(
-    model='gemini-2.5-flash',
-    api_key=api_key,
+    model="gemini-3.5-flash-lite",   # назва моделі
+    api_key=api_key    # ключ до сервера з моделлю
 )
 
+# Завдання 1
+# Напишіть модель для рекомендації книг з двох ланцюгів:
+#  Перший ланцюг отримує назву книги та визначає її
+# жанр
 
-# Користувач задає питання.
-# Потрібно дати відповіть та запропопонувати цікаві факти по тій
-# же темі що і питання
 
-# # варіант 1 -- все в один промпт
-# prompt = PromptTemplate.from_template(
-#     """
-#     Ти -- чатбот для навчання. Твоя задача давати відповідь на питання.
-#     Також потрібно запропонувати декілька цікавих фактів по тій же темі
-#     що і питання
+
 #
-#     ### Питання
-#     {question}
+# class GenreBooks(BaseModel):
+#     genre: str = Field(description="Жанр книги")
+#
+#
+# parser = PydanticOutputParser(pydantic_object=GenreBooks)
+#
+# instructions = parser.get_format_instructions()
+#
+# prompt_genre = PromptTemplate.from_template(
 #     """
+#     Ти -- чатбот-бібліотека
+#     Твоя задача дати визначити  жанр книги.
+#
+#
+#     ###ІНСТРУКЦІЇ###
+#     Відповідь має бути до 4 слів.
+#
+#     ###ФОРМАТ ВІДПОВІДІ###
+#     {format_instructions}
+#
+#     ###ВХІДНІ ДАНІ###
+#     {book}
+#
+#
+#     """,partial_variables={
+#         "format_instructions": instructions,
+#     }
 # )
 #
-# chain1 = prompt | llm
+# chain1 = prompt_genre | llm | parser
 #
-# response = chain1.invoke({
-#     "question": "Коли була висадка на місяць"
-# })
 #
-# print(response)
+# book_name = "Harry Potter"
+#
+#
+# data_book = {
+#     "book": book_name,
+# }
+#
+# response = chain1.invoke(data_book)
+#
+#
+# print(response.genre)
+#
+#
+# #  Другий отримує назву книги, жанр та повертає список
+# # схожих книг(того ж самого жанру та іншого)
+#
+# class  Recommendations(BaseModel):
+#     same_genre: list[str] = Field(description="Список книг того самого жанру")
+#     other_genre: list[str] = Field(description="Список схожих книг інших жанрів")
+#
+#
+# parser_books = PydanticOutputParser(pydantic_object= Recommendations)
+#
+# instructions_books = parser_books.get_format_instructions()
+#
+# prompt_books = PromptTemplate.from_template(
+#     """
+#     Ти — чатбот-бібліотека.
+#
+#     Твоя задача — рекомендувати книги, схожі на задану книгу.
+#
+#     ### ІНСТРУКЦІЇ ###
+#     1. Врахуй назву книги та її жанр.
+#     2. Поверни 5 книг того самого жанру.
+#     3. Поверни 5 схожих книг, але іншого жанру.
+#     4. Не рекомендуй саму задану книгу.
+#
+#     ### ФОРМАТ ВІДПОВІДІ ###
+#
+#     {format_instructions}
+#
+#     ###ВХІДНІ ДАНІ###
+#     {book}
+#     {genre}
+#     """,partial_variables=
+#     {
+#         "format_instructions": instructions_books,
+#     }
+# )
+#
+#
+# chain2 = prompt_books | llm | parser_books
+#
+#
+# book_name = "Harry Potter"
+#
+# data ={
+#     "book": book_name
+# }
+#
+# response1 = chain1.invoke(data)
+#
+# print(response1.genre)
+#
+#
+# data1 = {
+#     "book": book_name,
+#     "genre": response1.genre
+# }
+#
+# response2 = chain2.invoke(data1)
+#
+# print(response2)
 
 
-# варіант 2 -- розьити на 2 кроки
-# дати відповідь та визначити тему питання
-# згенерувати цікаві факти по темі
-
-# пишемо парсер
-
-# структура відповіді
-class ParserResult(BaseModel):
-    question_answer: str = Field(description="Answer to user question")
-    topics: List[str] = Field(description="список пов'язаних тем до питання")
 
 
-# створення парсера
-parser = PydanticOutputParser(pydantic_object=ParserResult)
+# Завдання 3
+# Напишіть модель для генерації резюме:
+#  Перший ланцюг отримує опис вакансії та повертає
+# основні навички, які необхідні
+#  Другий ланцюг отримує основні навички та опис
+# кандидата і генерує резюме
 
-# інструкція для llm як має виглядати відповідь
-instructions = parser.get_format_instructions()
+
+class Skills(BaseModel):
+    experience: float = Field(description="Досвід роботи в роках")
+    english_level: str = Field(description="Рівень англійської мови")
+    frameworks: list[str] = Field(description="Список бібліотек")
+    technologies: list[str] = Field(description="Список технологій")
+    language_programing : str = Field(description="Мова програмування")
+
+
+parser = PydanticOutputParser(pydantic_object=Skills)
+
+instrustions = parser.get_format_instructions()
 
 
 prompt = PromptTemplate.from_template(
     """
-    Ти -- чатбот для навчання. Твоя задача давати відповідь на питання.
-    Також потрібно визначити теми які відносяться до цього питання
-
-    ### Питання
-    {question}
+    Ти - досвідчений рекрутер.
+    Потрібно повернути основні навички з опису вакансії.
     
-    ### ФОРМАТ ВІДПОВІДІ
-    {instructions}
-    """,
-    partial_variables={"instructions": instructions}  # одразу передаємо інструкції
+    
+    ### ФОРМАТ ВІДПОВІДІ ###
+    {instrustions}
+
+    ###ВХІДНІ ДАНІ###
+     Опис вакансії: {vacancy_description}
+    
+
+    """,partial_variables={"instrustions":instrustions}
 )
 
-llm = llm.with_structured_output(ParserResult)
-chain = prompt | llm
 
-question = input("Введіть питання: ")
 
-response = chain.invoke({
-    "question": question,
-})
+chain = prompt | llm | parser
 
-print(f"Відповідь: {response.question_answer}")
 
-# # print(response)
-# # print(type(response))
-# #
-# # print(response.question_answer)
-# # print(response.topics)
-#
-# # генерація цікавих фактів на основі тем
-# class FactResponse(BaseModel):
-#     facts: List[str] = Field(description="список цікавих фактів розом з їхнім описом")
-#
-#
-# # створення парсера
-# parser = PydanticOutputParser(pydantic_object=FactResponse)
-#
-# # інструкція для llm як має виглядати відповідь
-# instructions = parser.get_format_instructions()
-#
-# prompt = PromptTemplate.from_template(
-#     """
-#     Ти -- генератор цікавих фактів. Твоя задача навести 5 цікавих фактів
-#     на задані теми
-#
-#     ### ТЕМИ
-#     {topics}
-#
-#     ### ФОРМАТ ВІДПОВІДІ
-#     {instructions}
-#     """,
-#     partial_variables={"instructions": instructions}  # одразу передаємо інструкції
-# )
-#
-# chain2 = prompt | llm | parser
-#
-# response = chain2.invoke(
-#     {
-#         "topics": response.topics
-#     }
-# )
-#
-# facts = response.facts
-#
-# print("Цікаві факти")
-# for fact in facts:
-#     print(fact)
-#
-# whole_chain = chain | chain2
+vacancy = """
+    Are you a Data Scientist with a love of LLMs, generative AI?
+ 
+We are looking for a passionate Data Scientist to implement AI solutions aimed at achieving business goals.
+ 
+This role offers the opportunity to work on cutting-edge AI adoption projects that helps to improve current business processes.
+ 
+     You'll be a great fit if you have:
+ Strong Python Experience (2 year +);
+Experience with LLM , Diffusion models;
+Knowledge of Prompt engineering;
+Experience with Gen AI-related technologies such as LangChain and RAG;
+Experience with Neural Networks (Optional) ;
+Experience with NLP , Predictive analytics and Machine learning;
+Experience with Pandas;
+Experience with SQL, including experience with large datasets;
+Strong experience in statistics;
+Bachelor's degree in Computer Science or a related field.
+What you'll do:
+Develop AI agents that utilize LLM, RAG and langchain approach;
+Implement LLM and Diffusion models to boost business productivity;
+Utilize LLM (LLM Vision) to improve object detection, text classification and extraction;
+Create forecasting, recommendation, and classification models;
+Transform business challenges to AI applications.
+
+     We ensure your growth with:
+Competitive salary fixed in USD;
+Flexible working schedule and fully remote work format;
+Paid vacation days and sick leave days ;
+Personal and professional development opportunities;
+Participation in building innovative projects from scratch using modern technologies;
+Team-building activities and corporate events;
+English classes and educational events.
+ 
+"""
+
+
+data = {
+    "vacancy_description": vacancy,
+}
+
+response = chain.invoke(data)
+print(response)

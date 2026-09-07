ff8bc2866c9ea93355f44b258352326347ae1f88
dima_kan <kandaurovdm@gmail.com>
2026-09-07 23:10:50 +0200

    7.09

---
+++ database6.py
@@ -0,0 +1,81 @@
+import uuid
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_core.documents import Document
+from uuid import uuid4
+from pinecone import ServerlessSpec
+from pinecone import Pinecone
+import dotenv
+import os
+import json
+
+
+dotenv.load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
+
+
+embedding = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-001",
+    api_key=api_key,
+)
+
+
+index_name = "data-task-6"
+
+pc = Pinecone(api_key=pinecone_api_key)
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=3072,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        ),
+    )
+
+index = pc.Index(index_name)
+
+vector_store = PineconeVectorStore(
+    index=index,
+    embedding=embedding
+)
+
+
+with open("data/lesson_rag/huge_file.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+
+ids = []
+blocks = text.split("\n\n\n")
+documents = []
+
+
+for block in blocks:
+
+    if block.strip():
+
+        document = Document(
+            page_content=block.strip()
+        )
+
+        documents.append(document)
+
+        ids.append(str(uuid4()))
+
+
+vector_store.add_documents(
+    documents=documents,
+    ids=ids
+)
+
+
+with open("data/lesson_rag/ids.json", "w", encoding="utf-8") as file:
+    json.dump(ids, file, ensure_ascii=False, indent=2)
+
+print(len(ids))
+print(len(documents))
---
+++ home-task6.py
@@ -0,0 +1,122 @@
+import dotenv
+import os
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from pinecone import ServerlessSpec
+from pinecone import Pinecone
+from langchain_core.messages import (
+    HumanMessage,
+    AIMessage,
+    SystemMessage,
+    BaseMessage,
+    trim_messages,
+)
+
+dotenv.load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash-lite",  # назва моделі
+    api_key=api_key  # ключ до сервера з моделлю
+)
+embedding = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-001",
+    api_key=api_key,
+)
+
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = "data-task-6"  # назва бази даних
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=3072,    # кількість чисел у векторі
+        metric="cosine",   # формула для пошуку схожих текстів
+        spec=ServerlessSpec(
+            cloud="aws",        # хмарна платформа(амазон)
+            region="us-east-1"  # регіон
+        ),
+    )
+
+index = pc.Index(index_name)
+
+vector_store = PineconeVectorStore(
+    index=index,          # база даних
+    embedding=embedding   # модель для кодування
+)
+
+@tool
+def document_search(query:str):
+    """
+    Пошук документів у векторній базі даних
+
+    База даних містить інформацію про користувальницьку угоду google
+    :param query:str -- запит користувача
+    :return: знайдені документи
+    """
+    result = vector_store.similarity_search(
+        query,
+        k=1
+    )
+    print(result)
+    return result
+
+
+
+agent = create_agent(
+    model=llm,  # нейромережа агента
+    tools=[document_search],  # список інструментів
+)
+
+messages = [
+    SystemMessage("""
+    Ти -- ввічливий чат бот
+
+    ###ІНСТРУКЦІЯ###
+    1.Якщо користувач питає про користувальницьку угоду то використовуй document_search
+    2.Якщо не має інформації в документах то нічого не вигадуй
+    """)
+]
+
+while True:
+    # Запит від користувача
+    user_query = input("Ви: ")
+
+    # умова закінчення
+    if user_query == "":
+        break
+
+    # зробити human message
+    user_message = HumanMessage(user_query)
+
+    # добавляємо повідомлення в історію
+    messages.append(user_message)
+
+    # отримати відповіть від агента
+    # агент сам дадає повідемлення в історію і повертає її
+
+    # агент треба передавати словник зі ключем "messages"
+    data = {
+        "messages": messages
+    }
+
+    data = agent.invoke(data)
+    # агент так само повертає словник
+
+    # дістаємо нову історію повідомлень
+    messages = data["messages"]
+
+    # відповідь моделі -- останнє повідомлення в історії
+    response = messages[-1]
+
+    # вивести відповідь на екран
+    print(response.text)
+
